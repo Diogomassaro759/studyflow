2124a49be696f11c7dd655d3f67a9aeeaaeba79b
Diogo Massaro <massarodouglas05@gmail.com>
2026-02-18 01:36:50 -0300

    fix: botão começar

--- app/page.tsx
+++ app/page.tsx
@@ -13,24 +13,24 @@ export default function Home() {
         gap: "20px",
       }}
     >
-      <h1>📚 StudyFlow</h1>
+      <h1>StudyFlow</h1>
 
       <p>Organize seus estudos.</p>
 
-      <Link href="/login">
-        <button
-          style={{
-            padding: "12px 24px",
-            fontSize: "16px",
-            borderRadius: "8px",
-            border: "none",
-            background: "#000",
-            color: "#fff",
-            cursor: "pointer",
-          }}
-        >
-          Começar
-        </button>
+      <Link
+        href="/login"
+        style={{
+          padding: "12px 24px",
+          fontSize: "16px",
+          borderRadius: "8px",
+          background: "#000",
+          color: "#fff",
+          cursor: "pointer",
+          textDecoration: "none",
+          display: "inline-block",
+        }}
+      >
+        Começar
       </Link>
     </main>
   );
